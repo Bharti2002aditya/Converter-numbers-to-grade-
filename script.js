146f41fea40957dc274517d0da6f447a5009899a
Aditya Bharti <119076885+Bharti2002aditya@users.noreply.github.com>
2023-11-25 02:38:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+let score = prompt("Write your persentage");
+
+
+
+if(score >= 85||score >= 90 ){
+
+    console.log("A+");
+
+    }else if(score >= 70||score >= 80 ){
+
+        console.log("B");
+
+        }
+
+        else if(score >= 60||score >= 70 ){
+
+        console.log("B+");
+
+        }
+
+        else if(score >= 50||score >= 60 ){
+
+        console.log("C");
+
+        }
+
+        else if(score >= 40||score >= 50 ){
+
+        console.log("D");
+
+        }
+
+        else if(score >= 30||score >= 40 ){
+
+        console.log("E");
+
+        }
